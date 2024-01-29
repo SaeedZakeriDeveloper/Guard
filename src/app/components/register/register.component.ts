13b6a4c7b5c0c70649d3294be6babe37d948b420
Saeed <saeed.zakeri1364@gmail.com>
2024-01-29 22:10:13 +0330

    develop

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {IUser} from 'src/app/interfaces/api-interface';
 import {UserService} from 'src/app/service/user.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -11,30 +12,27 @@ export class RegisterComponent {
 
   user: IUser = {id: 0, name: '', email: '', password: ''};
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router, private routes: ActivatedRoute) {
   }
 
   onAddUser(name: string, email: string, password: string) {
-    this.user.id = 0;
-    this.user.name = name;
-    this.user.email = email;
-    this.user.password = password;
-
-    this.userService.add(this.user).subscribe((res) => {
-        if (res == true) {
-          alert('success')
-        } else {
-          alert("Error")
-        }
+    this.userService.get().subscribe((users) => {
+      if (users.length > 0) {
+        let ids = users.map(u => u.id);
+        let maxId = Math.max(...ids);
+        this.user.id = maxId + 1;
+      } else {
+        this.user.id = 1;
       }
-    )
+      this.user.name = name;
+      this.user.email = email;
+      this.user.password = password;
 
+      this.userService.add(this.user).subscribe((res) => {
+        alert('success');
+        this.router.navigate([''], {relativeTo: this.routes});
+      });
+    });
   }
+
 }
-
-
-
-
-
-
-
